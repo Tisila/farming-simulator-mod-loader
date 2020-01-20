50b7eec62e5f07f84026251143e86628b06f92a3
tisila44 <tiagoavila44@gmail.com>
2020-01-20 18:23:21 +0000

    Implemented first version of the mod loader.
    Added some prints for information.
    Implemented big files download as a function.
    Added headers to access cdn server correctly.
    Created .exe file with pyinstaller.

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # farming-simulator-mod-loader
 A simple mod list downloader for farming simulator.
+
+## Preparation
+The script only supports mod pages from *www.farming-simulator.com*.
+This file must be in the same directory as the script. There is an example mods file in the repository.
+Create a file named **fsmods.txt** with one link in each line.
+
+## Running
+The script looks for the **fsmods.txt** file and parses it as links, it retrieves the download link and downloads the *.zip* file.
+After editing the script, double-click the *.exe* file and you're good to go.
+
+##TLDR
+Just clone or download the zip, edit the *.txt* file with your favorite mods and run the script, *.exe* or *.py*.
---
+++ fsmodloader.py
@@ -0,0 +1,54 @@
+import os
+import requests
+from bs4 import BeautifulSoup
+
+#File to read mod links from
+links_file = 'fsmods.txt'
+
+#Help for this in here https://www.geeksforgeeks.org/downloading-files-web-using-python/
+def get_mod(source, filename, headers):
+	wsize = 0
+	response = requests.get(source, stream = True, headers = headers)
+	with open(filename, 'wb') as zipfile:
+		for chunk in response.iter_content(chunk_size=1024):
+			if chunk:
+				wsize += zipfile.write(chunk)
+	print('Downloaded {} bytes in file {}\n'.format(wsize, filename))
+
+#Check if file doesn't exist
+if not os.path.isfile(links_file):
+    print('The file {} does not exist is the current directory\nPlease rename your file.'.format(links_file))
+else:
+    print('Found mods file, parsing mods list')
+    #Reading lines and cleaning them
+    with open(links_file) as f:
+        links = f.readlines()
+    links = [x.strip() for x in links]
+
+    print('{} mods to download\n'.format(len(links)))
+
+    #Downloading each mod
+    for link in links:
+        download_headers = {
+            'Host': 'cdn30.giants-software.com',
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:72.0) Gecko/20100101 Firefox/72.0',
+            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+            'Accept-Language': 'en-US,en;q=0.5',
+            'Accept-Encoding': 'gzip, deflate, br',
+            'Connection': 'keep-alive',
+            'Referer': link,
+            'Upgrade-Insecure-Requests': '1',
+            'Pragma': 'no-cache',
+            'Cache-Control': 'no-cache'}
+        #Retrieving the mod landing page for mod download cdn link 
+        r = requests.get(link)
+        soup = BeautifulSoup(r.text, 'html.parser')
+        html = list(soup.children)[2]
+        body = list(html.children)[3]
+        btn = body.find_all(class_='button-buy')[0]
+        mod_link = btn['href']
+        mod_name = mod_link.split('/')[-1]
+        #Downloading the mod
+        print('Downloading {}'.format(mod_link))
+        get_mod(mod_link, mod_name, download_headers)
+    print('Download complete!')
