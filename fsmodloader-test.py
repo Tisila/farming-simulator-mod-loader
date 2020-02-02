e3c05a127d685c73dea7626a0aa7e8adcf2b122e
Trevor Collins <trevor@y720-pop-os.lan>
2020-02-02 23:56:14 +0000

    Started new mod object.
    The object will now hold usefull data.
    Added necessary unit tests.

---
+++ fsmodloader-test.py
@@ -0,0 +1,48 @@
+import re
+import unittest
+from datetime import datetime
+from fsmodloader import Mod
+
+class TestMod(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls._mod = Mod('https://farming-simulator.com/mod.php?lang=en&country=us&mod_id=148173&title=fs2019')
+
+    def test_mod_webpage(self):
+        self.assertEqual(self._mod._webpage, 'https://farming-simulator.com/mod.php?lang=en&country=us&mod_id=148173&title=fs2019')
+
+    def test_mod_link(self):
+        regex_pattern = re.compile('https://cdn*.giants-software.com/modHub/storage/00148173/FS19_DairyBarn.zip')
+        self.assertTrue(regex_pattern.fullmatch(self._mod._link))
+
+    def test_mod_size(self):
+        self.assertEqual(self._mod._size, 13.53)
+
+    def test_mod_unit(self):
+        self.assertEqual(self._mod._unit, 'MB') 
+
+    def test_mod_filename(self):
+        self.assertEqual(self._mod._filename, 'FS19_DairyBarn.zip')
+
+    def test_mod_is_downloaded(self):
+        self.assertFalse(self._mod.is_downloaded())
+        
+    def test_mod_is_updated(self):
+        self.assertFalse(self._mod.is_updated())
+
+class  TestModPriorToDownload(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls._mod = Mod('https://farming-simulator.com/mod.php?lang=en&country=us&mod_id=148173&title=fs2019')
+        cls._mod.download()
+
+    def test_mod_is_updated(self):
+        self.assertTrue(self._mod.is_updated())
+
+    def test_mod_is_downloaded(self):
+        self.assertTrue(self._mod.is_downloaded())
+
+if __name__ == '__main__':
+    unittest.main()
--- fsmodloader.py
+++ fsmodloader.py
@@ -6,55 +6,96 @@ from bs4 import BeautifulSoup
 #File to read mod links from
 links_file = 'fsmods.txt'
 
-#Help for this in here https://www.geeksforgeeks.org/downloading-files-web-using-python/
-def get_mod(source, filename, headers, size):
-    wsize = 0
-    response = requests.get(source, stream = True, headers = headers)
-    with open(filename, 'wb') as zipfile:
-        for chunk in response.iter_content(chunk_size=1024):
-            if chunk:
-                wsize += zipfile.write(chunk)
-                wsize_MB = wsize/1024/1024
-                sys.stdout.write('Downloaded {:.2f}/{:.2f} MB in file {}\r'.format(wsize_MB, size, 'filename'))
-                sys.stdout.flush()
-    print('Downloaded {:.2f}/{:.2f} MB in file {}\n'.format(wsize_MB, size, 'filename'))  
+class Mod:
+    def __init__(self, webpage):
+        """
+        Defines a mod object with the webpage link to retrieve data.
 
-#Check if file doesn't exist
-if not os.path.isfile(links_file):
-    print('The file {} does not exist is the current directory\nPlease rename your file.'.format(links_file))
-else:
-    print('Found mods file, parsing mods list')
-    #Reading lines and cleaning them
-    with open(links_file) as f:
-        links = f.readlines()
-    links = [x.strip() for x in links]
+        :param webpage: Website link as string
+        """
+        self._webpage = webpage
+        self._link = ""
+        self._size = 0
+        self._unit = ""
+        self._filename = ""
+        self.get_information()
 
-    print('{} mods to download\n'.format(len(links)))
+    def is_updated(self):
+        return False
 
-    #Downloading each mod
-    for link in links:
-        download_headers = {
-            'Host': 'cdn30.giants-software.com',
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:72.0) Gecko/20100101 Firefox/72.0',
-            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
-            'Accept-Language': 'en-US,en;q=0.5',
-            'Accept-Encoding': 'gzip, deflate, br',
-            'Connection': 'keep-alive',
-            'Referer': link,
-            'Upgrade-Insecure-Requests': '1',
-            'Pragma': 'no-cache',
-            'Cache-Control': 'no-cache'}
-        #Retrieving the mod landing page for mod download cdn link 
-        r = requests.get(link)
+    def is_downloaded(self):
+        return os.path.isfile(self._filename)
+
+    def get_information(self):
+        """
+        Retrieves the object data from the webpage.
+        """
+        #Retrieving the mod landing page for mod download information
+        r = requests.get(self._webpage)
         soup = BeautifulSoup(r.text, 'html.parser')
         html = list(soup.children)[2]
         body = list(html.children)[3]
         btn = body.find_all(class_='button-buy')[0]
-        mod_link = btn['href']
-        mod_name = mod_link.split('/')[-1]
-        size_text = body.find_all(class_='table-cell')[9].get_text()
-        size = float(size_text.split(' ')[0])
-        #Downloading the mod
-        print('Downloading {}'.format(mod_link))
-        get_mod(mod_link, mod_name, download_headers, size)
-    print('Download complete!')
+        self._link = btn['href']
+        self._filename = self._link.split('/')[-1]
+        size_data = body.find_all(class_='table-cell')[9].get_text().split(' ')
+        self._size = float(size_data[0])
+        self._unit = size_data[1] 
+
+    def download(self):
+        """
+        Downloads the mod zip through the object data.
+        """
+        #Defining headers
+        download_headers = {
+                'Host': 'cdn30.giants-software.com',
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:72.0) Gecko/20100101 Firefox/72.0',
+                'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+                'Accept-Language': 'en-US,en;q=0.5',
+                'Accept-Encoding': 'gzip, deflate, br',
+                'Connection': 'keep-alive',
+                'Referer': self._webpage,
+                'Upgrade-Insecure-Requests': '1',
+                'Pragma': 'no-cache',
+                'Cache-Control': 'no-cache'}
+        #Download file
+        #Help for this in here https://www.geeksforgeeks.org/downloading-files-web-using-python/
+        wsize = 0
+        response = requests.get(self._link, stream = True, headers = download_headers)
+        with open(self._filename, 'wb') as zipfile:
+            for chunk in response.iter_content(chunk_size=1024):
+                if chunk:
+                    wsize += zipfile.write(chunk)
+                    wsize_MB = wsize/1024/1024
+                    m = 'Downloaded {:.2f}/{:.2f} MB in file {}\r'
+                    sys.stdout.write(m.format(wsize_MB, self._size, 'filename'))
+                    sys.stdout.flush()
+        #Download complete
+        m = 'Downloaded {:.2f}/{:.2f} MB in file {}\n'
+        print(m.format(wsize_MB, self._size, 'filename'))  
+
+if __name__=="__main__":
+    #Check if file doesn't exist
+    if not os.path.isfile(links_file):
+        m = 'The file {} does not exist in the current directory\nPlease rename your file.'
+        print(m.format(links_file))
+    else:
+        print('Found mods file, parsing mods list')
+        #Reading lines and cleaning them
+        with open(links_file) as f:
+            links = f.readlines()
+        links = [x.strip() for x in links]
+
+        print('{} mods to download\n'.format(len(links)))
+
+        #Downloading each mod
+        mods = []
+        for link in links:
+            mods.append(Mod(link))
+
+        #Downloading the mods
+        for mod in mods:
+            if not mod.updated:
+                print('Downloading {}'.format(mod_link))
+                mod.download(mod_link, mod_name, download_headers, size)
+        print('Download complete!')
